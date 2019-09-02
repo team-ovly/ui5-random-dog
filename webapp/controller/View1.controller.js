3e77464653e6ec655103efbaab3e294615501fc7
Fábio Pagoti <fabiopagoti@gmail.com>
2019-09-02 14:13:37 +0000

    first version working with images

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -4,8 +4,30 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("ovly.random-dog.controller.View1", {
+		
+		endpoint: "https://random.dog/woof.json",
+		
 		onInit: function () {
-
+			this.callAPI();	
+		},
+		
+		onGetRandomDog: function(oEvent){
+			this.callAPI();
+		},
+		
+		callAPI: function(){
+			// var callback = $.proxy(this._updatePicture); // Using jQuery
+			var callback = this._updatePicture.bind(this); // Using vanilla JS
+			
+			$.get(this.endpoint, callback);
+		},
+		
+		_updatePicture: function(result){
+			this.byId("container").setSrc(result.url);
 		}
+		
+		
+		
+		
 	});
 });
