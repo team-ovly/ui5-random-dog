c36690b396b748463f75b78c2048b44fa0e1d217
Fábio Pagoti <fabiopagoti@gmail.com>
2019-09-02 15:54:21 +0000

    ends with Polyfill

--- webapp/Component.js
+++ webapp/Component.js
@@ -25,6 +25,19 @@ sap.ui.define([
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
+
+			// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+			if (!String.prototype.endsWith)
+				String.prototype.endsWith = function (searchStr, Position) {
+					// This works much better than >= because
+					// it compensates for NaN:
+					if (!(Position < this.length))
+						Position = this.length;
+					else
+						Position |= 0; // round position
+					return this.substr(Position - searchStr.length,
+						searchStr.length) === searchStr;
+				};
 		}
 	});
 });
