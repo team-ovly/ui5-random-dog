e16b0be2b9cacc418fd8d5952de1063a8b5ef81a
Fábio Pagoti <fabiopagoti@gmail.com>
2019-09-02 14:51:03 +0000

    handling dog videos

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,6 +7,8 @@ sap.ui.define([
 		
 		endpoint: "https://random.dog/woof.json",
 		
+		imageContainerId: "image-container",
+		
 		onInit: function () {
 			this.callAPI();	
 		},
@@ -17,15 +19,41 @@ sap.ui.define([
 		
 		callAPI: function(){
 			// var callback = $.proxy(this._updatePicture); // Using jQuery
-			var callback = this._updatePicture.bind(this); // Using vanilla JS
+			var callback = this._updateContainer.bind(this); // Using vanilla JS
 			
 			$.get(this.endpoint, callback);
 		},
 		
-		_updatePicture: function(result){
-			this.byId("container").setSrc(result.url);
-		}
+		_updateContainer: function(result){
+			var isVideo = result.url.endsWith("mp4");
+			
+			if(isVideo){
+				this._showVideo(result.url);
+			}else{
+				this._showImage(result.url);
+			}
 		
+		},
+		
+		_showVideo: function(sUrl){
+			var video = $("video");
+			video.prop("hidden", false);
+			this.byId(this.imageContainerId)
+				.setVisible(false);
+			
+			var source = video.find("source");
+			source.attr("src", sUrl);
+			video.load();
+		},
+		
+		_showImage: function(sUrl){
+			var video = $("video");
+			video.prop("hidden", true);
+			
+			this.byId(this.imageContainerId)
+				.setVisible(true)
+				.setSrc(sUrl);
+		}
 		
 		
 		
